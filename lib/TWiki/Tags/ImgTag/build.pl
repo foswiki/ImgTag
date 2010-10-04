2d76942d4ae29a78c676bf623ea44c4c11bd6aef
GeorgeClark <GeorgeClark@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-10-04 15:45:29 +0000

    Item9774: update build.pl so that build will use the extension module for RELEASE/VERSION strings
    git-svn-id: http://svn.foswiki.org/trunk/ImgTag@9499 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/TWiki/Tags/ImgTag/build.pl
+++ lib/TWiki/Tags/ImgTag/build.pl
@@ -31,7 +31,7 @@ our @ISA = qw( TWiki::Contrib::Build );
 
 sub new {
     my $class = shift;
-    return bless( $class->SUPER::new( "ImgTag", "Build" ), $class );
+    return bless( $class->SUPER::new( "ImgTag" ), $class );
 }
 
 # Example: Override the build target
